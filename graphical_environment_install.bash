ffbb554c6b378a6e92d299674015012ec611ec32
lucioric <lucioric2000@hotmail.com>
2018-07-20 02:24:15 -0500

    Update graphical_environment_install.bash

--- graphical_environment_install.bash
+++ graphical_environment_install.bash
@@ -3,5 +3,5 @@
 yum groupinstall "X Window System"
 yum install gnome-classic-session gnome-terminal nautilus-open-terminal control-center liberation-mono-fonts
 #If you want default access to the graphical windows, you may execute the commands in the following 2 lines:
-#unlink /etc/systemd/system/default.target
-#ln -sf /lib/systemd/system/graphical.target /  /etc/systemd/system/default.target
+mv /etc/systemd/system/default.target /etc/systemd/system/nongraphic.target
+ln -sf /lib/systemd/system/graphical.target /  /etc/systemd/system/default.target
