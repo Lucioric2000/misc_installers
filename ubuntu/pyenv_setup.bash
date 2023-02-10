e9d5b2693f0bf90648f874fe061d4dc9b11d6354
lucioric <lucioric2000@hotmail.com>
2023-02-09 22:18:59 -0600

    Update pyenv_setup.bash

--- ubuntu/pyenv_setup.bash
+++ ubuntu/pyenv_setup.bash
@@ -1,2 +1,9 @@
 	
 sudo apt-get install -y make build-essential git libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
+# follow the instructions in https://github.com/pyenv/pyenv.git
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+Add the commands to bashrc with the commands:
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+to add to ~/.bash_profile:
