4b3ed5cfc1bd506e4f422ed85abcfd8fca6d9468
lucioric <lucioric2000@hotmail.com>
2023-02-09 22:25:56 -0600

    Update pyenv_setup.bash

--- ubuntu/pyenv_setup.bash
+++ ubuntu/pyenv_setup.bash
@@ -6,4 +6,11 @@ Add the commands to bashrc with the commands:
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-to add to ~/.bash_profile:
+to add to ~/.profile:
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+ggggg
+to add the pyenv-virtualenv plugin:
+$ git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+$ echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
